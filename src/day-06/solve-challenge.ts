e304aecbd9936f36732e573fdeab76160bfef944
jlyden1 <jlyden1@alumni.nd.edu>
2025-12-18 19:28:28 -0500

    Progress on day 6 part 2, but whitespace matters

--- src/day-06/solve-challenge.ts
+++ src/day-06/solve-challenge.ts
@@ -1,34 +1,17 @@
 import { getContents, multiplyNumericArray, sumNumericArray } from "../utils";
 
-// node dist/day-06/solve-challenge.js <sample|input>
+// node dist/day-06/solve-challenge.js <sample|input> <2>
 function solveChallenge() {
 	const input = getInput(process.argv[2]);
 	const [numberLines, operations] = parseInput(input);
 
-	const results: number[] = []
-
-	const problemCount = operations.length;
-	const lineCount = numberLines.length;
-
-	for (let problem = 0; problem < problemCount; problem++) {
-		const elements: number[] = []
-
-		for (let line = 0; line < lineCount; line++) {
-			const element = Number(numberLines[line][problem]);
-			elements.push(element)
-		}
-
-		let result = 0;
-		if (operations[problem] === '*') {
-			result = multiplyNumericArray(elements);
-		} else {
-			result = sumNumericArray(elements);
-		}
-
-		results.push(result);
+	let solution = 0;
+	if (process.argv[3] === '2') {
+		solution = runPartTwo(numberLines, operations);
+	} else {
+		solution = runPartOne(numberLines, operations);
 	}
 
-	const solution = sumNumericArray(results);
 	console.log(`Solution is: ${solution}`);
 }
 
@@ -61,6 +44,70 @@ function parseInput(input: string[]): [RegExpMatchArray[], RegExpMatchArray] {
 	}
 
 	return [numberLines, operations];
+}
+
+function runPartOne(numberLines: RegExpMatchArray[], operations: RegExpMatchArray): number {
+	const results: number[] = []
+
+	const problemCount = operations.length;
+	const lineCount = numberLines.length;
+
+	for (let problem = 0; problem < problemCount; problem++) {
+		const elements: number[] = []
+
+		for (let line = 0; line < lineCount; line++) {
+			const element = Number(numberLines[line][problem]);
+			elements.push(element)
+		}
+
+		const result = mathElements(elements, operations[problem]);
+		results.push(result);
+	}
+
+	return sumNumericArray(results);
+}
+
+// Incomplete, b/c the whitespace matters
+function runPartTwo(numberLines: RegExpMatchArray[], operations: RegExpMatchArray): number {
+	const results: number[] = []
+
+	const problemCount = operations.length;
+	const lineCount = numberLines.length;
+
+	for (let problem = 0; problem < problemCount; problem++) {
+		const values: string[][] = []
+
+		let longestValue = 0;
+		for (let line = 0; line < lineCount; line++) {
+			const value = numberLines[line][problem];
+			values.push(value.split(''));
+			if (value.length > longestValue) {
+				longestValue = value.length;
+			}
+		}
+		
+		const newElements: number[] = [];
+		for (let i = 0; i < lineCount; i++) {
+			let element: string[] = [];
+			for (let j = 0; j < longestValue; j++) {
+				const char = values[j].pop();
+				if (char) {
+					element.push(char);
+				}
+			}
+			newElements.push(Number(element.join('')));
+		}
+
+		console.log('newElements: ', newElements);
+		const result = mathElements(newElements, operations[problem]);
+		results.push(result);
+	}
+
+	return sumNumericArray(results);
+}
+
+function mathElements(elements: number[], operation: string): number {
+	return operation === '*' ? multiplyNumericArray(elements) : sumNumericArray(elements);
 }
 
 solveChallenge();
